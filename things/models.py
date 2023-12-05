add31b813e0f6a68a2d2cfeb174b40c771a15856
Diwas Rai <diwas.rai@kcl.ac.uk>
2023-12-05 10:07:58 +0000

    remove quantity field

--- things/models.py
+++ things/models.py
@@ -6,6 +6,3 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True, unique=False)
-    quantity = (
-        models.IntegerField(),
-    )
