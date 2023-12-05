37a8ec1ea2e849b0cc2ea1bf18d730f088f94c41
Diwas Rai <diwas.rai@kcl.ac.uk>
2023-12-05 10:06:58 +0000

    remove refinements from quantity field

--- things/models.py
+++ things/models.py
@@ -7,9 +7,5 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True, unique=False)
     quantity = (
-        models.IntegerField(
-            blank=True,
-            unique=False,
-            validators=[MinValueValidator(0), MaxValueValidator(100)],
-        ),
+        models.IntegerField(),
     )
