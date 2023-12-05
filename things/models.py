f44b9456b3cd3159b0421a2f8361c3ac654520a1
Diwas Rai <diwas.rai@kcl.ac.uk>
2023-12-05 10:10:26 +0000

    add quantity field and refine

--- things/models.py
+++ things/models.py
@@ -6,3 +6,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(
+        unique=False,
+        blank=False,
+        validators=[MinValueValidator(0), MaxValueValidator(1000)],
+    )
